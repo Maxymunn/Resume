0ad80c12326ca161bf83a7f642a51fab8cd93e9e
Maxymunn <maxymunn@gmail.com>
2017-05-19 17:48:43 -0300

    Add files via upload

--- Resume/js/resumeBuilder.js
+++ Resume/js/resumeBuilder.js
@@ -1,10 +1,39 @@
+var skills = ["HTML5", "CSS3", "JavaScript", "UI"];
+var bio = {
+	"name" : "Rodrigo Max Felipe",
+	"age" : 26,
+	"skills" : skills,
+	"role" : "Web Developer",
+	"email" : "maxymunn@gmail.com"
+};
+
+
 $(function() {
-	var email = "maxymunn@gmail.com";
-	var name = "Rodrigo Max Felipe";
-	var role = "Web Developer";
-	var formattedName = HTMLheaderName.replace("%data%", name);
-	var formattedRole = HTMLheaderRole.replace("%data%", role);
+	var formattedName = HTMLheaderName.replace("%data%", bio.name);
+	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 
 	$("#header").prepend(formattedRole)
 	$("#header").prepend(formattedName);
+
+	if($(".flex-item").length === 0) {
+		$("#topContacts").hide();
+	}
+	if($("h1").length == 0) {
+		$("header").hide();
+	}
+	if($(".work-entry").length === 0) {
+		$("#workExperience").hide();
+	}
+	if($(".project-entry").length === 0) {
+		$("#projects").hide();
+	}
+	if($(".education-entry").length === 0) {
+		$("#education").hide();
+	}
+	if($(".flex-item").length === 0) {
+		$("#lets-connect").hide();
+	}
+	if($("#map") === null) {
+		$("#mapDiv").hide();
+	}
 });
